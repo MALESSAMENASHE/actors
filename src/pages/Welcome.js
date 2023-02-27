495e9775ac6fba9a0519390b9159be8532b539bc
menashe <immmalessa@gmail.com>
2023-02-27 10:07:39 +0200

    css

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -4,10 +4,16 @@ import { Link } from 'react-router-dom'
 const Home = () => {
     return (
         <section>
+            <div className='welcome'>
             <h1>Welcome to The Guessing Game</h1>
-            <h3>You have to guess whether the actor in the picture played in the written movie.</h3>
+            <br></br>
+            <h3>You have to guess whether the actor in the picture played in the written movie.
+            <br></br>
+            You need 6 correct answers to win.
+            </h3>
             <br></br>
             <Link to={'/game'}>Continue to game...</Link>
+            </div>
         </section>
     )
 }
